f98c638a131dee244d4da9479783dd5ff528dca0
gabereal <gaberealmusic@gmail.com>
2014-06-25 07:13:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sequiapp::Application.config.secret_key_base = 'fe047d4bbad78a46c9aa3cd8392b7c8ed0573203f10a5c58b3f82b8aaf3f126bae0ac6c768b9c968991f8b8341e7603ca480a1848a8d0f49bedc2dc7c4e9b977'
